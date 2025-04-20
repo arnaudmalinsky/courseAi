5c346ffdf2b59110a45002670fd8e11d12969138
arnaudmalinsky <arnaud.malinsky@gmail.com>
2025-04-20 05:51:07 -0300

    english prompt

---
+++ prompting/english_initial_text_cleaning_prompt.py
@@ -0,0 +1,4 @@
+PROMPT = """Can you improve the layout of this text, without omitting any information, creating clearer paragraphs and sentences, italicizing foreign language terms, converting numbers into figures, improving the presentation of important legal concepts, but keeping all the content, without using too many bullet points with enumeration and without creating new headings than those mentioned?
+No text should be added after or before the text provided.
+
+Here's the text:'"""
